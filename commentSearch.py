c057ae16809c52836c863996a56fed80635a5d82
Chiara Zitarosa <chiara.zitarosa@hotmail.com>
2023-05-15 21:10:02 +0200

    added all keywords, refactoring, seperated post and comment search, still printing stuff double but replies work with keywords

--- commentSearch.py
+++ commentSearch.py
@@ -3,7 +3,7 @@ import csv
 
 with open('postSearch.csv', mode='w', encoding='utf-8', newline='') as file:
     csv_writer = csv.writer(file)
-    csv_writer.writerow((['Index', 'Title', 'Text', 'Comments']))
+    # csv_writer.writerow(['Index', 'Title', 'Text', 'Comments'])
 
     # creates a reddit instance
     reddit = praw.Reddit(client_id='HFe49kpE24lryrjVxKvl4g',
@@ -15,16 +15,26 @@ with open('postSearch.csv', mode='w', encoding='utf-8', newline='') as file:
     # select a subreddit you want to analyze
     subreddit = reddit.subreddit('robotics')
 
+
     # select the keyword(s) you want to look for in the subreddit
     # leave empty if you want to search all comments in the subreddit
-    keywordsSubreddit = ['trust', 'reliable']
+
+    # keywordsSubreddit = ['trust', 'reliable', 'predictable', 'consistent', 'capable', 'skilled', 'competent',
+    #                      'dependable', 'capacity', 'performance', 'ethical', 'respectable', 'principled',
+    #                      'sincere', 'genuine', 'authentic', 'cheat', 'secure', 'fair', 'candid', 'moral',
+    #                      'help', 'aid', 'rely', 'trustworthy', 'honest']
+
+    keywordsSubreddit = []
 
     # select the keyword(s) you want to look for in the comments
     # leave empty if you want all comments from a post
-    keywordsComments = []
+    keywordsComments = ['trust']
 
     # false if you need 'robot' as an extra search word, true if you only look for the keywords
-    robotic_subreddit = False
+    robotic_subreddit = True
+
+    #false if comments should be analyzed true if posts should be analyzed
+    post_analysis = False
 
     # Checks if the keyword(s) are in the comment/replies
     # Returns a list with the matching comment/replies
@@ -32,49 +42,55 @@ with open('postSearch.csv', mode='w', encoding='utf-8', newline='') as file:
 
         comment_list = []
 
-        if all(keyword in input_comment.body for keyword in keywordsComments):
+        if any(keyword in input_comment.body for keyword in keywordsComments):
             comment_list.append(input_comment.body)
-        for reply in input_comment.replies:
-            if all(keyword in reply.body for keyword in keywordsComments):
-                comment_list += get_comments_from_post(reply)
+            for reply in input_comment.replies:
+                if any(keyword in reply.body for keyword in keywordsComments):
+                    comment_list += get_comments_from_post(reply)
+
+
+
 
         return comment_list
 
-
     # goes through all the posts with the keyword(s) and prints its comments
 
-    limitAll = None
-    limitKeywords = None
+    def collect_posts(empty_list):
+
+        if not keywordsSubreddit:  # if you want to search for comments in the entire subreddit
+            empty_list = subreddit.new(limit=200)
+        else:  # if you want to search for posts
+            if robotic_subreddit:
+                for keyword123 in keywordsSubreddit:
+                    empty_list.extend(subreddit.search(keyword123, limit=None))
+            else:
+                for keyword123 in keywordsSubreddit:
+                    empty_list.extend(subreddit.search(f"{keyword123} AND robot", limit=None))
+        return empty_list
+
 
     posts = []
-
-    if not keywordsSubreddit:  # if you want to search for comments in the entire subreddit
-        posts = subreddit.new(limit=limitAll)
-    else:  # if you want to search for posts
-        if robotic_subreddit:
-            for keyword123 in keywordsSubreddit:
-                posts.extend(subreddit.search(keyword123, limit=limitAll))
-        else:
-            for keyword123 in keywordsSubreddit:
-                posts.extend(subreddit.search(f"{keyword123} AND robot", limit=limitAll))
-
-
+    posts = collect_posts(posts)
 
     counter = 0
 
-    for item in posts:
+    if post_analysis:
 
-        post = reddit.submission(item.id)
-        post.comments.replace_more(limit=None)
+        for item in posts:
 
-        list_of_comments = []
+            post = reddit.submission(item.id)
+            counter += 1
+            csv_writer.writerow([counter, post.title, post.selftext])
+    else:
+        for item in posts:
+            post = reddit.submission(item.id)
+            post.comments.replace_more(limit=None)
 
-        for comment in post.comments.list():
-            list_of_comments.extend(get_comments_from_post(comment))
+            list_of_comments = []
 
-        counter += 1
-
-        # csv_writer.writerow([counter, post.title, post.selftext, *list_of_comments])
-        csv_writer.writerow([counter, post.title])
+            for comment in post.comments.list():
+                list_of_comments.extend(get_comments_from_post(comment))
+            if len(list_of_comments) > 0:
+                csv_writer.writerow([*list_of_comments])
 
 file.close()
